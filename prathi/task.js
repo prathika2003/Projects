7ddc3274bb9bf45a7327f5e029b8dce3fb95f49a
prathika2003 <prathikaramesh04@gmail.com>
2026-02-16 19:22:05 +0530

    Add files via upload

---
+++ prathi/task.js
@@ -0,0 +1,27 @@
+// function prathi(str) {
+//     return str.split('').reverse().join(''); 
+// }
+
+// const str = "prathi";
+// console.log(prathi(str));
+
+// const a=10;
+// let b=20;
+// var c=30;
+
+// function task(){
+//     const a=10;
+// let b=20;
+// var c=30;
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+// }
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// task();
+var a;          // Declaration is hoisted and initialized to `undefined`
+console.log(a); // Output: undefined
+a = 10;         // Assignment happens here
